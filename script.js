b1b019756bf896ec44b9a79e08c3d1de5cedc4b3
sauravpatel77s <sauravpatel220930@acropolis.in>
2024-08-28 15:20:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// let n = 5;
+
+// for(let i=0;i<n;i++){
+//     console.log("hello world",i);
+// }
+
+// console.log("bye");
+
+let args = process.argv;
+
+for(let i=2;i<args.length;i++){
+    console.log("hello to ",args[i]);
+}
